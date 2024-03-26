c4c4d254b19febeee58740c4f5ff6795becdfcc5
mateuszprask <mateusz.prask@gmail.com>
2024-03-26 19:32:20 +0100

    update lekcja 3

---
+++ cypress/e2e/login.js
@@ -0,0 +1,24 @@
+import { Login } from "../pages/Login";
+import { HomePage } from "../pages/HomePage";
+import { afterEach, beforeEach } from "node:test";
+
+const loginPage = new Login();
+const homePage = new HomePage();
+
+describe("Zadanie 3.", () => {
+  beforeEach(() => {
+    loginPage.visit();
+  });
+
+  afterEach(() => {
+    homePage.logout();
+  });
+
+  it("Log in with user888@gmail.com and 1234567890", () => {
+    loginPage.login("user888@gmail.com", "1234567890");
+  });
+
+  it("Log in with testowyqa@qa.team and QA!automation-1", () => {
+    loginPage.login("testowyqa@qa.team", "QA!automation-1");
+  });
+});
---
+++ cypress/pages/HomePage.js
@@ -0,0 +1,6 @@
+export class HomePage {
+  logout() {
+    cy.get("#open-navigation-menu-mobile").click();
+    cy.contains("Log out").click();
+  }
+}
