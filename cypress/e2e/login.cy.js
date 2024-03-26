00cc06c1b755d3bb86df1afeaeb16432b5da229b
mateuszprask <mateusz.prask@gmail.com>
2024-03-26 19:43:07 +0100

    .

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,15 +5,19 @@ const loginPage = new Login();
 const homePage = new HomePage();
 
 describe("Zadanie 3.", () => {
-  it("Log in with user888@gmail.com and 1234567890", () => {
+  beforeEach(() => {
     loginPage.visit();
-    loginPage.login("user888@gmail.com", "1234567890");
+  });
+
+  afterEach(() => {
     homePage.logout();
   });
 
+  it("Log in with user888@gmail.com and 1234567890", () => {
+    loginPage.login("user888@gmail.com", "1234567890");
+  });
+
   it("Log in with testowyqa@qa.team and QA!automation-1", () => {
-    loginPage.visit();
     loginPage.login("testowyqa@qa.team", "QA!automation-1");
-    homePage.logout();
   });
 });
